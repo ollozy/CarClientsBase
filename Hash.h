5996c87c03a4bfc6aa37a2b9d3ea70ae4c4d1b29
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-13 20:20:44 +0300

    Обновлен класс Hash<>
    Удалены ненужные заголовки
    Удалены комментарии с битой кодировкой
    Класс приведен к шаблонному
    Удалены ненужные фукнции

--- Hash.h
+++ Hash.h
@@ -1,72 +1,53 @@
 #ifndef HASH_H
 #define HASH_H
 
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
+#include <cstring>
 
-const char deletedSegment[7] = "*****\0";
-const char emptySegment[7] = "     \0";
-
+template<int KeyLen, typename Val>
 class Hash
-{
+{    
+    static const char deletedSegment[KeyLen] = "*****\0";
+    static const char emptySegment[KeyLen] = "     \0";
+
+    struct Data {
+        Data() : m_val(0), m_key(new char[KeyLen]) { std::strncpy(m_key, KeyLen, emptySegment); }
+        ~Data() { delete[] m_key; delete m_val; }
+        Val *m_val;
+        char* m_key;
+    };
+
+    using uint = unsigned int;
+
 public:
     Hash()
-        : m_elements(new Data[m_capacity])
-        , m_size(0)
+        : m_size(0)
         , m_capacity(3000)
-        , m_multiCoef(m_capacity / 102) {}
+        , m_multiCoef(m_capacity / 102)
+        , m_elements(new Data[m_capacity]) {}
     ~Hash() { delete[] m_elements; }
-    //Вставка значение value по ключу key
-    void insert(const char* key, const int& value); 
-    //Удаление значения по ключу key
-    bool erase(const char* key); 
-    //Перегруженный метод. Удаление по номеру сегмента
-    bool erase(const unsigned int seg);
-    //Полная очистка табилцы
+
+    void insert(const char* key, const Val& value);
+    bool erase(const char* key);
+
     void clear();
-    //Получения значения по ключу
     int operator[](const char* key) const;
-    //Получение значения по сегменту
-    int operator[](const unsigned int seg) const;
-    //Выгружает все ключи в переданный массив
-    void keys(char(*keysStorage)[7]) const;
-    //Получение ключа по номеру сегмента
-    char* key(const unsigned int seg) const { return m_elements[seg].m_key; }
-    //Содержит ли таблица переданный ключ
+
+    void keys(char(*keysStorage)[KeyLen]) const;
     bool hasKey(const char* key) const;
-    //Значащий размер таблицы
-    int size() const { return m_size; }
-    //Фактичекй размер таблицы
-    int capacity() const { return m_capacity; }
-    bool testDataFile(const char *fileName) const;
+
+    int size() const;
+    int capacity() const;
 
 private:
-    //Увеличение фактического размера таблицы
     void resize(unsigned int size = 0);
-    //Хеш-функция
-    unsigned int hashFunction(const char* key) const;
-    //Функция квадратичного опробования
-    void quadTesting(int tryNum, unsigned int& seg) const { seg += 1 * tryNum + 1 * tryNum * tryNum; }
-    //Структура для хранения данных элемента hash таблицы
-    struct Data {
-        //стандартный конструктор
-        Data() : m_val(0), m_key(new char[7]), m_segCalcCount(0) { std::strcpy(m_key, emptySegment); }
-        //Чтобы небыло утечек при удалении
-        ~Data() { delete[] m_key; }
-        //Поля значения и ключа
-        int m_val;
-        char* m_key;
-        unsigned int m_segCalcCount;
-    };
-    //Значащий размер таблицы
-    unsigned int m_size;
-    //Фактический размер таблицы
-    unsigned int m_capacity;
-    //Множитель размера таблицы
-    unsigned int m_multiCoef;
+    uint hashFunction(const char* key) const;
+    void linearTesting(int tryNum, unsigned int& seg) const;
 
-    //Массив элементов
+private:
+    uint m_size;
+    uint m_capacity;
+    uint m_multiCoef;
+
     Data* m_elements;
 };
 
