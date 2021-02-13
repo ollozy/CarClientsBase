df143100235189c2dd7037bac24a68fc3085922a
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-13 23:54:20 +0300

    Обновлены фукнции clear, key, hasKey, size, capacity и resize

--- Hash.h
+++ Hash.h
@@ -54,14 +54,16 @@ public:
     void clear();
     int operator[](const char* key) const;
 
-    void keys(char(*keysStorage)[KeyLen]) const;
-    bool hasKey(const char* key) const;
+
+    void keys() const;
+    const char *key(const Val &val) const;
+    bool hasKey(const char *key) const;
 
     int size() const;
     int capacity() const;
 
 private:
-    void resize(unsigned int size = 0);
+    void resize(uint newSize);
     uint hashFunction(const char* key) const;
     void linearTesting(int tryNum, unsigned int& seg) const;
 
@@ -127,21 +129,77 @@ void Hash<KeyLen, Val>::erase(const char *key)
     }
 }
 
-template<int KeyLen, typename Val>
+template<uint KeyLen, typename Val>
 void Hash<KeyLen, Val>::clear()
 {
     for(uint seg = 0; seg < m_capacity; ++seg) {
-        if(std::strcmp(m_elements[seg].m_key, deletedSegment) != 0
-                && std::strcmp(m_elements[seg].m_key, emptySegment) != 0) {
-            std::strcmp(m_elements[seg].m_key, deletedSegment);
-            delete m_elements[seg].m_val;
+        if(std::strncmp(m_elements[seg].m_key, deletedSegment, KeyLen) != 0
+                && std::strncmp(m_elements[seg].m_key, emptySegment, KeyLen) != 0) {
+            std::strncmp(m_elements[seg].m_key, deletedSegment, KeyLen);
             --m_size;
         }
     }
-    m_capacity = 3000;
+    m_capacity = initCapacity;
     delete[] m_elements;
-    m_elements = new Data[m_capacity]{Val()};
-    m_multiCoef = m_capacity / 102;
+    m_elements = new Data[m_capacity];
+    m_multiCoef = static_cast<uint>(m_capacity / (maxHashFunc - minHashFunc));
+}
+template<uint KeyLen, typename Val>
+}
+const char *Hash<KeyLen, Val>::key(const Val &val) const
+{
+    for(int i = 0; i < m_capacity; ++i) {
+        if(val == m_elements[i].m_val)
+            return m_elements[i].m_key;
+    }
+    return "\0";
+}
+
+template<uint KeyLen, typename Val>
+bool Hash<KeyLen, Val>::hasKey(const char *key) const
+{
+    uint seg = hashFunction(key);
+     for(uint i = 0; seg < m_capacity; ++i) {
+         if(std::strncmp(key, m_elements[seg].m_key, KeyLen) == 0)
+             return true;
+         else
+             linearTesting(i, seg);
+     }
+     return false;
+}
+
+template<uint KeyLen, typename Val>
+int Hash<KeyLen, Val>::size() const
+{
+    return m_size;
+}
+
+template<uint KeyLen, typename Val>
+int Hash<KeyLen, Val>::capacity() const
+{
+    return m_capacity;
+}
+
+template<uint KeyLen, typename Val>
+void Hash<KeyLen, Val>::resize(uint newSize)
+{
+    Data *temp = new Data[newSize];
+    for(uint i = 0; i < m_capacity; ++i) {
+        std::strncpy(temp[i].m_key, m_elements[i].m_key, KeyLen);
+        temp[i].m_val = m_elements[i].m_val;
+    }
+    delete[] m_elements;
+    m_elements = new Data[newSize];
+    m_capacity = newSize;
+    m_size = 0;
+    for(uint i = 0; i < newSize; ++i) {
+        if(std::strncmp(temp[i].m_key, deletedSegment, KeyLen) != 0
+                && std::strncmp(temp[i].m_key, emptySegment, KeyLen) != 0) {
+            insert(temp[i].m_key, temp[i].m_val);
+        }
+    }
+    delete[] temp;
+}
 
 template<uint KeyLen, typename Val>
 unsigned int Hash<KeyLen, Val>::hashFunction(const char *key) const
