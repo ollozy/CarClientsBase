168ea536f9449b0e43bed4a5ef7fa970b07925ee
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-13 17:04:53 +0300

    Удалены коментарии с битой кодировкой, добавлено описание задания, подключен файл связанного списка

--- LinkList.h
+++ LinkList.h
@@ -1,10 +1,11 @@
-#pragma once
-#include <iostream>
+#ifndef LINK_LIST
+#define LINK_LIST
+
+#include <QObject>
 
 template<typename T> class LinkList;
 template<typename Node> class ListIterator;
 
-//Шаблонный класс для хранения элементов и их указателей
 template<typename T>
 class node
 {
@@ -17,14 +18,13 @@ public:
 
 	~node() { delete m_data; }
 
-	//функции получения значений
+    node *previous() const { return m_previous; }
 
-	//указатуль на следующий элемент в списке
+    node *next() const { return m_next; }
 	node* next() const { return m_next; }
-	//даные
 	Value* data() const { return m_data; }
 
-	//функции установки значений
+    Value *data() { return m_data; }
 	void setData(Value* data) { m_data = data; }
 	void setNext(node* next) { m_next = next; }
 
@@ -33,7 +33,6 @@ private:
 	Value* m_data;
 };
 
-//Класс итератора для упращения обхода элементов
 template<typename Node>
 class ListIterator
 {
@@ -42,7 +41,7 @@ public:
 
 	ListIterator(Node* data = nullptr) : m_ptr(data) {}
 
-	//Перегруженные операторы
+    ListIterator &operator=(const ListIterator &other)
 	//operator Node() { return m_ptr; }
 	ValueType& operator*() { return *m_ptr->data(); }
 	Node* operator->() { return m_ptr; }
@@ -67,14 +66,13 @@ public:
 		return *this; 
 	}
 
-	//Чтобы не добавлять класс как дружественный в LinkList<T>, функция объявлена как public
+        ListIterator temp = *this;
 	Node* _getPtr() const { return m_ptr; }
 
 private:
 	Node* m_ptr;
 };
 
-//Класс циклического списка
 template<typename T> 
 class LinkList
 {
@@ -84,7 +82,6 @@ public:
 	using iterator = ListIterator<Node>;
 
 	LinkList() : m_begin(nullptr), m_size(0) {}
-	//Конструктор копирования
 	LinkList(const LinkList<T>& li)
 	{
 		LinkList<T>::iterator it = li.begin();
@@ -99,8 +96,6 @@ public:
 		_CrtDumpMemoryLeaks();
 	}
 
-	//служебные методы возвращают размер списка, 
-	//проверку на пустоту списка и итератор на начало соответственно
 	int size() const { return m_size; }
 	bool isEmpty() const { return !m_size; }
 	iterator begin() const { return iterator(m_begin); }
@@ -133,18 +128,18 @@ public:
 			if (*it++ == *thisIt++) isEqual = false;
 		return isEqual;
 	}
-	/*
-	* Произовдит вставку в связанный список после элемента,
-	* на который указывает итератор pos
-	* Возвращает итератор, указывающий на вставленный элемент
-	*/
+        else {
+            *m_end->data() = val;
+            Node *newEnd = new Node(new T(), nullptr, m_end);
+            m_end->setNext(newEnd);
+            m_end = newEnd;
 	iterator insert(iterator pos, const T& elem)
 	{
-		//Проверка недействительность переданного итератора
-		//Если итератор недействителен и список пуст, то итератор указывает 
-		//на начало списка и необходимо создание первого элемента
-		//Иначе передан недействительный итератор (функция возвращает 
-		//переданный недействительный итератор)
+        if(isEmpty()) {
+            *m_begin->data() = val;
+            m_end = new Node(new T(), nullptr, m_begin);
+            m_begin->setNext(m_end);
+        }
 		if (!pos) {
 			if (!isEmpty())
 				return pos;
@@ -153,17 +148,12 @@ public:
 			return iterator(m_begin);
 		}
 
-		//Вставка нового элемена 
 		pos->setNext(new Node(new T(elem), pos->next()));
 		++m_size;
 		return iterator(pos->next());
 	}
 
-	/*
-	* Производит удаление элемента следующего за тем, на который указывает итератор pos из списка
-	* Возвращает итератор на элемент перед удаляемым
-	* Передаваемый в функцию итератор становится недействительным
-	*/
+    iterator insert(const T& val, iterator iter)
 	iterator erase_after(iterator pos)
 	{
 		if (pos->next() == m_begin) {
@@ -177,34 +167,26 @@ public:
 		return iterator(iter);
 	}
 
-	/*
-	* Производит удаление элемента, на который указывает итератор pos из списка
-	* Возвращает итератор на элемент после удаляемого
-	* Передаваемый в функцию итератор становится недействительным
-	*/
 	iterator erase(iterator pos)
 	{
-		//проверяем, является ли элемент первым
-		//если это так передвигаем указатель на начало на один элемент вперед
 		if (pos == begin())
 			m_begin = m_begin->next();
 
 		Node* iter = pos._getPtr();
-		//поиск элемент предшествующий данному
+        //            m_begin = m_begin->next();
 		while (pos != iter->next())
 			iter = iter->next();
 
-		//Сохранение элемента следующего за данным
+        //        Node* iter = pos._getPtr();
 		Node* newNext = iter->next()->next();
-		//Удаляем переданный в функцию элемент
+        //            iter = iter->next();
 		delete iter->next();
-		//Связываем разорванный список
+        //        delete iter->next();
 		iter->setNext(newNext);
 		--m_size;
 		return iterator(newNext);
 	}
 
-	//очищает связанный список
 	void clear()
 	{
 		Node* iter = m_begin;
@@ -220,24 +202,19 @@ public:
 		}
 	}
 
-	/*
-	* Производит поиск элемента elem в связанном списке
-	* Возвращает итератор на (нужно чтобы на предидущий) найденный элемент, если элемент elem есть в списке
-	* Иначе возвращает итератор на начало списка
+        //            Node* newNext = iter->next()->next();
+        //            delete iter->next();
+        //            iter->setNext(newNext);
+        //            --m_size;
 	*/
 	iterator find(const T& elem)
 	{
-		//если списко пуст поиск не имеет смысла
 		if (isEmpty())
 			return begin();
-		//Если искомый элемент равен первому возвратить его
 		if (elem == *m_begin->data())
 			return begin();
-		//Итераторы для обхода списка
 		iterator beg = begin();
 		iterator it = beg;
-		//В цикле с передаваемым значением сравниваются все элементы кроме первого
-		//Если элемент не найден, возвращается итератор на начало списка
 		while (++it != beg) {
 			if(*it == elem)
 				return it;
@@ -249,3 +226,4 @@ private:
 	Node* m_begin;
 	int m_size;
 };
+#endif //LINK_LIST
