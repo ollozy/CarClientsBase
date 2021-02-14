6a63b57f7f361b042677e732c25ced562a901720
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-14 16:46:02 +0300

    Метод insert заменен на рекурсивный, добавлена балансировка дерева

--- SearchTree.h
+++ SearchTree.h
@@ -3,7 +3,6 @@
 
 #include <typeinfo>
 #include <cstring>
-#include <iostream>
 
 template<typename Val, uint KeyLen>
 class Tree {
@@ -11,14 +10,60 @@ class Tree {
     typedef unsigned int uint;
 
     struct Node {
-        Node(Val data = Val(), const char *key = nullptr, Node *right = nullptr, Node *left = nullptr)
+        Node(const Val &data = Val(), const char *key = nullptr, Node *right = nullptr, Node *left = nullptr)
             : m_data(data)
             , m_key(new char[KeyLen])
+            , m_height(1)
             , m_rightChild(right)
             , m_leftChild(left) { if(key) std::strncpy(m_key, key, KeyLen); }
         ~Node() { delete[] m_key; }
+        int balanceFactor() const
+        {
+            uint leftHeight = m_leftChild ? m_leftChild->m_height : 0;
+            uint rightHeight = m_rightChild ? m_rightChild->m_height : 0;
+
+            return rightHeight - leftHeight;
+        }
+        void fixHeight()
+        {
+            uint leftHeight = m_leftChild ? m_leftChild->m_height : 0;
+            uint rightHeight = m_rightChild ? m_rightChild->m_height : 0;
+
+            m_height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+        }
+        Node *rotateLeft() {
+            Node *rotNode = m_rightChild;
+            m_rightChild = rotNode->m_leftChild;
+            rotNode->m_leftChild = this;
+            fixHeight();
+            rotNode->fixHeight();
+            return rotNode;
+        }
+        Node *rotateRight() {
+            Node *rotNode = m_leftChild;
+            m_leftChild = rotNode->m_rightChild;
+            rotNode->m_rightChild = this;
+            fixHeight();
+            rotNode->fixHeight();
+            return rotNode;
+        }
+        Node *balance() {
+            fixHeight();
+            if(balanceFactor() == 2) {
+                if(m_rightChild->balanceFactor() < 0)
+                    m_rightChild = m_rightChild->rotateRight();
+                return rotateLeft();
+            }
+            else if(balanceFactor() == -2) {
+                if(m_leftChild->balanceFactor() > 0)
+                    m_leftChild = m_leftChild->rotateLeft();
+                return rotateRight();
+            }
+            return this;
+        }
         Val m_data;
         char *m_key;
+        uint m_height;
         Node *m_rightChild;
         Node *m_leftChild;
     };
@@ -32,6 +77,23 @@ public:
     void erace(const char *key);
     void clear();
 
+private:
+    Node *insertNode(const char *key, Node *node, const Val &val)
+    {
+        if(std::strncmp(key, node->m_key, KeyLen) < 0) {
+            if(node->m_leftChild != nullptr)
+                node->m_leftChild = insertNode(key, node->m_leftChild, val);
+            else
+                node->m_leftChild = new Node(val, key);
+        }
+        else if(std::strncmp(key, node->m_key, KeyLen) > 0) {
+            if(node->m_rightChild != nullptr)
+                node->m_rightChild = insertNode(key, node->m_rightChild, val);
+            else
+                node->m_rightChild = new Node(val, key);
+        }
+        return node->balance();
+    }
 
 private:
     Node *m_rootNode;
@@ -40,27 +102,12 @@ private:
 template<typename Val, uint KeyLen>
 void Tree<Val, KeyLen>::insert(const char *key, const Val &val)
 {
-    Node *newNode = new Node(val, key);
     if(!m_rootNode) {
-        m_rootNode = newNode;
+        m_rootNode = new Node(val, key);
         return;
     }
-    Node *searchNode = m_rootNode;
-    while((std::strncmp(key, searchNode->m_key, KeyLen) > 0 && searchNode->m_rightChild)
-          || (std::strncmp(key, searchNode->m_key, KeyLen) < 0 && searchNode->m_leftChild)) {
-        if(std::strncmp(key, searchNode->m_key, KeyLen) > 0)
-            searchNode = searchNode->m_rightChild;
-        else if(std::strncmp(key, searchNode->m_key, KeyLen) < 0)
-            searchNode = searchNode->m_leftChild;
-    }
-    if(std::strncmp(key, searchNode->m_key, KeyLen) > 0)
-        searchNode->m_rightChild = newNode;
-    else if(std::strncmp(key, searchNode->m_key, KeyLen) < 0)
-        searchNode->m_leftChild = newNode;
+    m_rootNode = insertNode(key, m_rootNode, val);
+    return;
 }
-
-
-
-
 
 #endif // SEARCHTREE_H
