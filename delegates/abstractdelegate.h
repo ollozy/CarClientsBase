bdb08461a6138aa439a11e2611629c1d96c4e952
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-23 18:53:08 +0300

    Добавлено объявление класса AbstractDelegate

--- delegates/abstractdelegate.h
+++ delegates/abstractdelegate.h
@@ -1,11 +1,26 @@
 #ifndef ABSTRACTDELEGATE_H
 #define ABSTRACTDELEGATE_H
 
+#include "../app_core/modelindex.h"
+
+class AbstractItemModel;
 
 class AbstractDelegate
 {
 public:
     AbstractDelegate();
+    virtual ~AbstractDelegate();
+
+    virtual void draw() = 0;
+
+    const ModelIndex &currentIndex() const;
+    void setCurrentIndex(const ModelIndex &index);
+
+    AbstractItemModel *model() const;
+    void setModel(AbstractItemModel *model);
+
+private:
+    AbstractItemModel *m_model;
 };
 
 #endif // ABSTRACTDELEGATE_H
