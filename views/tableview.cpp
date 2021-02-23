ca39813c8df860e7df9dc7f3c6aaff60d2faee0b
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-23 21:29:22 +0300

    TableView теперь не выделяет хидер и порядковый номер

--- views/tableview.cpp
+++ views/tableview.cpp
@@ -31,3 +31,11 @@ void TableView::update()
         }
     }
 }
+
+void TableView::selectItem(int row, int column)
+{
+    if(row == 0 || column == 0)
+        return;
+
+    AbstractItemView::selectItem(row, column);
+}
