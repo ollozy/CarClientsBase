61b1bbef0d26dd2a10614dfc12d272cb92434bbc
AndreevDmitriy <g.andreevm.d@gmailcom>
2021-02-23 19:02:24 +0300

    Добавлено объявление класса AbstractItemModel

--- models/abstractitemmodel.h
+++ models/abstractitemmodel.h
@@ -1,11 +1,25 @@
 #ifndef ABSTRACTITEMMODEL_H
 #define ABSTRACTITEMMODEL_H
 
+#include "../app_core/modelindex.h"
 
 class AbstractItemModel
 {
 public:
     AbstractItemModel();
+    virtual ~AbstractItemModel();
+
+    virtual const char *data() const = 0;
+    virtual void setData(const char *data, const ModelIndex &index) = 0;
+
+    virtual void insertColumn(int column) = 0;
+    virtual void removeColumn(int column) = 0;
+    virtual void insertRow(int row) = 0;
+    virtual void removeRow(int row) = 0;
+    virtual void columnCount() const = 0;
+    virtual void rowCount() const;
+
+    virtual void setHeaderData(const char *data, int column);
 };
 
 #endif // ABSTRACTITEMMODEL_H
